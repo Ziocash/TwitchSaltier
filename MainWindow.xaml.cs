afa9c4e29c37575e555a9559283bbd6a896486a6
Ziocash <31541164+Ziocash@users.noreply.github.com>
2019-10-12 14:14:30 +0200

    Project update
    [+] Added a new feature that allows to select a faeout timer for message
    [+] Typo corrections

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
+using TwitchSaltier.Properties;
 
 namespace TwitchSaltier
 {
@@ -21,12 +11,33 @@ namespace TwitchSaltier
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly string SaltString = "PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt";
+        private const string SaltString = "PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt";
+        private DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
-            timer.Interval = TimeSpan.FromSeconds(10);
+            if (Settings.Default.TimerInterval > TimeSpan.FromSeconds(0))
+            {                
+                SelectCmbValue(Settings.Default.TimerInterval.Seconds);
+                timer.Interval = Settings.Default.TimerInterval;
+            }
+            else
+            {
+                SelectCmbValue(timer.Interval.Seconds);
+                timer.Interval = Settings.Default.TimerPreselectedInterval;
+            }
             timer.Tick += Timer_Tick;
+        }
+
+        private void SelectCmbValue(int seconds)
+        {
+            if (seconds == 3)
+                cmbTimer.SelectedIndex = 0;
+            else if (seconds == 5)
+                cmbTimer.SelectedIndex = 1;
+            else
+                cmbTimer.SelectedIndex = 2;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -35,14 +46,19 @@ namespace TwitchSaltier
             timer.Stop();
         }
 
-        private DispatcherTimer timer = new DispatcherTimer();
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Clipboard.Clear();
             Clipboard.SetText(SaltString);
             txbLogger.Text = "Copied to clipboard!";
             timer.Start();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ComboBoxItem value = (ComboBoxItem)cmbTimer.Items[cmbTimer.SelectedIndex];
+            Settings.Default.TimerInterval = TimeSpan.FromSeconds(int.Parse(value.Content.ToString()));
+            Settings.Default.Save();
         }
     }
 }
